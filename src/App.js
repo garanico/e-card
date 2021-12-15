44d10ecc205a923d4f1b931689e65a70fd2da719
Grace Aranico <grace.aranico@gmail.com>
2021-12-15 11:19:05 -0800

    bug(card path): fix card path from email link

--- src/App.js
+++ src/App.js
@@ -8,45 +8,48 @@ import GreetingCard from './components/GreetingCard';
 import Preview from './components/Preview';
 
 function App() {
-  const [greeting, setGreeting] = useState('');
-  const [body, setBody] = useState('');
-  const [closing, setClosing] = useState('');
+  const query = new URLSearchParams(window.location.search);
+  const [importedGreeting, setImportedGreeting] = useState(query.get("greeting") || "Dear Harry,");
+  const [importedBody, setImportedBody] = useState(query.get("body") || "I hope you are having a Happy Christmas!");
+  const [importedClosing, setImportedClosing] = useState(query.get("closing") || "Cheers, Hagrid");
   const [email, setEmail] = useState('');
 
+  console.log( query.get("greeting") );
 
   return (
     <HashRouter>
       <Routes>
         <Route exact path="/" element={
           <CardForm
-          greeting={greeting}
-          setGreeting={setGreeting}
-          body={body}
-          setBody={setBody}
-          closing={closing}
-          setClosing={setClosing}
-          email={email}
-          setEmail={setEmail} />
+          greeting={importedGreeting}
+          setGreeting={setImportedGreeting}
+          body={importedBody}
+          setBody={setImportedBody}
+          closing={importedClosing}
+          setClosing={setImportedClosing}
+          email={email} 
+          setEmail={setEmail}
+          />
         }/>
         <Route exact path="/preview" element={
           <Preview 
-          greeting={greeting}
-          setGreeting={setGreeting}
-          body={body}
-          setBody={setBody}
-          closing={closing}
-          setClosing={setClosing}
+          greeting={importedGreeting}
+          setGreeting={setImportedGreeting}
+          body={importedBody}
+          setBody={setImportedBody}
+          closing={importedClosing}
+          setClosing={setImportedClosing}
           email={email}
           />
         }/>
         <Route exact path="/card" element={
           <GreetingCard
-          greeting={greeting}
-          setGreeting={setGreeting}
-          body={body}
-          setBody={setBody}
-          closing={closing}
-          setClosing={setClosing}
+          greeting={importedGreeting}
+          setGreeting={setImportedGreeting}
+          body={importedBody}
+          setBody={setImportedBody}
+          closing={importedClosing}
+          setClosing={setImportedClosing}
           email={email}
         />
         }/>
